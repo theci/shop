5d81b1c73f7fc291f4129c7ee6522594c942677e
창훈 <p16331751@gmail.com>
2023-01-17 16:04:51 +0900

    프로젝트 다시 만들었음

---
+++ shop/shop/src/test/java/com/example/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.shop.repository;
+
+import com.example.shop.constant.ItemSellStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = new Item();
+        item.setItemNM("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
+}
