019bd8415d7a339c069002098c8daa6b9b90afb6
창훈 <p16331751@gmail.com>
2023-01-22 01:36:42 +0900

    스프링 시큐리티를 이용한 회원 가입 및 로그인 기능 추가

---
+++ shop/shop/src/main/java/com/example/shop/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.shop.repository;
+
+import com.example.shop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String Email);
+}
